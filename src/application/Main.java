7a13844d67fee6f3b81439539b471caf79e9543d
Eric dos Reis Alves <eralves01@hotmail.com>
2021-11-17 08:55:03 -0300

    criação no menu da MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,33 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene scene = new Scene(scrollPane);
+			stage.setScene(scene);
+			stage.setTitle("Scena do Projeto JavaFX com JDBC");
+			stage.show();
+		}
+		catch (IOException e) {
+		e.printStackTrace();
 		}
 	}
 	
